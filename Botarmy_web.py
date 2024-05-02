bd35d14eda2b2d124cd172ba4c558703c1f35f2f
Imanolasolo <jjusturi@gmail.com>
2024-05-02 09:32:39 -0500

    Botarmy web content fix

--- Botarmy_web.py
+++ Botarmy_web.py
@@ -11,7 +11,7 @@ with col2:
     st.title('BOTARMY-HUB: Virtual assistants for professionals & companies')
 
 # Subtítulo
-st.subheader('Welcome to the future of automation')
+st.subheader('Welcome to the future of automation with AI powered virtual assistants')
 
 # Menú de navegación
 menu = ['Home', 'About us', 'Services', 'Contact']
@@ -22,16 +22,14 @@ if opcion == 'Home':
     code_message ="""
     Welcome to BOTARMY-HUB, where cutting-edge virtual assistants meet your business needs. 
     We're not just about chatbots; we're transforming how businesses operate in the digital age. 
-    Our dedicated team crafts personalized solutions to streamline workflows, boost productivity, 
-    and enhance customer experiences. 
-    Whether you're a startup or an enterprise, BOTARMY-HUB has the expertise to help you thrive. 
     Join us at the forefront of automation and unlock your business's full potential today."""
     st.success(code_message)
 
 # Página "Sobre Nosotros"
 elif opcion == 'About us':
-    code_message ="""
-    Welcome to BOTARMY-HUB, where innovation drives excellence. Our passionate team of tech enthusiasts pushes the boundaries of virtual assistant technology. We harness the power of AI and automation to empower professionals and businesses worldwide. With a focus on creativity, reliability, and exceptional customer service, we deliver cutting-edge solutions that revolutionize business operations. Our mission: provide clients with the tools to thrive in the digital landscape. Join us on this journey of innovation, and let's build the future together with BOTARMY-HUB."""
+    code_message ="""    
+    Our mission: provide clients with the tools to thrive in the digital landscape. 
+    Join us on this journey of innovation, and let's build the future together with BOTARMY-HUB."""
     
     st.warning(code_message)
 
@@ -56,10 +54,10 @@ elif opcion == 'Contact':
 
     st.warning(code_message)
     
-    col1, col2, col3, col4 =st.columns(4)
+    col1, col2, col3 =st.columns(3)
     with col1:
         st.image('mail_icon.png', width=80)
-        st.markdown('<a href="mailto:jjusturi@gmail.com">Send me a mail</a>', unsafe_allow_html=True)
+        st.markdown('<a href="mailto:jjusturi@gmail.com">Send us a mail</a>', unsafe_allow_html=True)
            
     with col2:
         st.image('whatsapp_logo.png', width=100)
@@ -68,9 +66,7 @@ elif opcion == 'Contact':
     with col3:
         st.image('meeting_icon.png', width=100)
         st.markdown('<a href="https://buymeacoffee.com/imanolasolo">Let`s have a coffee and have a consultation about technical issues or Coach & Coffee!</a>', unsafe_allow_html=True)
-    with col4:
-        st.image('linkedin_logo.png', width=80)
-        st.markdown('<a href="https://www.linkedin.com/in/imanolasolo/">Find me on Linkedin!</a>', unsafe_allow_html=True)
+    
 
 # Menú de navegación II
 menu2 = ['Chat with us', 'Want to be a Bot reseller?']
@@ -79,7 +75,11 @@ opcion = st.sidebar.selectbox('Interact with us', menu2)
 if opcion == 'Chat with us':
     st.write("You selected to chat with us")
     # Redireccionar al usuario a un enlace específico
-    st.markdown("[Go to chat](https://botarmy-chat.streamlit.app/)")
+    col1,col2 = st.columns([1,4])
+    with col1:
+        st.image('bot_icon.png',width=100)
+    with col2:
+        st.markdown("[Go to chat](https://botarmy-chat.streamlit.app/)")
 
 if opcion == 'Want to be a Bot reseller?':
     st.write("You selected to how to resell our bots")
